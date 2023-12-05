cf59b02ec62e787369173e8e4fae3295a05d4163
Andi Sina <andy.sina32@gmail.com>
2023-12-05 17:28:04 +0100

    Transaction

--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -1,10 +1,22 @@
 class TransactionsController < ApplicationController
   def new
+    current_user
     @transaction = Transaction.new
   end
 
   def create
-    @sender_wallets = Wallet.where(user_id: current_user)
-    @receiver_wallets = Wallet.where.not(user_id: @current_user.id)
+    @transaction = Transaction.new(transaction_params)
+    @transaction.sender_id = Wallet.where(user_id: @current_user)
+    @transaction.receiver_id = params(:transaction['receiver_id'])
+    # @transaction.receiver_id = Wallet.find(params[:receiver_id])
+    raise
+    @transaction.save
+    # @receiver_wallets = Wallet.where.not(user_id: @current_user.id)
+  end
+
+  private
+
+  def transaction_params
+    params.require(:transaction).permit(:amount, :sender_id, :receiver_id)
   end
 end
--- app/models/wallet.rb
+++ app/models/wallet.rb
@@ -1,4 +1,7 @@
 class Wallet < ApplicationRecord
   belongs_to :user
+  # has_many :transactions, dependent: :destroy
+  has_many :transactions_as_sender, class_name: "Transaction", foreign_key: :sender_id
+  has_many :transactions_as_receiver, class_name: "Transaction", foreign_key: :receiver_id
   validates :name, :currency, :main, presence: true
 end
