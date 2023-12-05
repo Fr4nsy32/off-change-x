c801ea276600fdbcf19f62890a53182e6c91aa6e
vanessa.x.marchiori@gmail.com <vanessa.x.marchiori@gmail.com>
2023-12-05 15:47:07 +0100

    added transaction controller

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -1,2 +1,7 @@
 class Transaction < ApplicationRecord
+  validates :amount, presence: true, numericality: { greater_than: 0 }
+  validates :sender_id, presence: true
+  validates :receiver_id, presence: true
+  belongs_to :sender, class_name: 'Wallet', foreign_key: 'sender_id'
+  belongs_to :receiver, class_name: 'Wallet', foreign_key: 'receiver_id'
 end
