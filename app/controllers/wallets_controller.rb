ab84a23a65a70749e6a0669f0c74b5241cf773f1
Matt Robinson <matt.jack.robinson@gmail.com>
2023-12-05 12:19:05 +0000

    setting up wallets controllers and views

--- app/controllers/wallets_controller.rb
+++ app/controllers/wallets_controller.rb
@@ -1,2 +1,53 @@
 class WalletsController < ApplicationController
+
+  def index
+    @wallets = Wallet.all
+  end
+
+  def show
+    @wallet = Wallet.find(params[:id])
+  end
+
+  def new
+    current_user
+    @wallet = Wallet.new
+  end
+
+  def create
+    @wallet = Wallet.new(wallet_params)
+    @wallet.user = current_user
+    if @wallet.save
+      redirect_to @wallet, notice: "Wallet was successfully created."
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def edit
+    current_user
+    @wallet = Wallet.find(params[:id])
+  end
+
+  def update
+    current_user
+    @wallet = Wallet.find(params[:id])
+    if @wallet.update(wallet_params)
+      redirect_to @wallet, notice: "Wallet was successfully updated."
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    current_user
+    @wallet = Wallet.find(params[:id])
+    @wallet.destroy
+    redirect_to wallets_path, notice: "Wallet was successfully destroyed."
+  end
+
+  private
+
+  def wallet_params
+    params.require(:wallet).permit(:name, :currency, :main)
+  end
 end
