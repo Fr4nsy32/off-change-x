35b25262bf75f381b7a5add3f348c0f1d00151a5
Andi Sina <andy.sina32@gmail.com>
2023-12-13 18:25:14 +0100

    Pages not working

--- config/routes.rb
+++ config/routes.rb
@@ -12,9 +12,6 @@ Rails.application.routes.draw do
   get "exchange", to: "pages#exchange", as: 'exchange'
 
   resources :payments, only: [:new, :create]
-  get '/404', to: 'errors#not_found'
-  get '/500', to: 'errors#internal_server'
-  get '/422', to: 'errors#unprocessable'
 
   resources :currencies, only: [:new, :create]
   resources :stores, only: [:index, :show, :new, :create, :edit, :update, :destroy]
