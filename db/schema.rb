0a14e2b5279ad11ff70429676655b6e70bb5f316
Andi Sina <andy.sina32@gmail.com>
2023-12-14 14:24:40 +0100

    Transaction unique_code

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2023_12_07_135602) do
+ActiveRecord::Schema[7.1].define(version: 2023_12_14_125748) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -37,6 +37,7 @@ ActiveRecord::Schema[7.1].define(version: 2023_12_07_135602) do
     t.datetime "updated_at", null: false
     t.integer "status", default: 0
     t.float "rate"
+    t.string "unique_code"
     t.index ["receiver_id"], name: "index_transactions_on_receiver_id"
     t.index ["sender_id"], name: "index_transactions_on_sender_id"
   end
