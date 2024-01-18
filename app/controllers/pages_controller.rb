4bf2666e5c42e64915d2451b077883cf9073ca44
Andi Sina <andy.sina32@gmail.com>
2024-01-18 11:18:49 +0100

    Chart and rates on homepage

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -5,7 +5,7 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [:home, :exchange, :currency]
 
   def home
-@store=Store.near(current_user())
+    @store=Store.near(current_user())
   end
 
   def index
@@ -19,24 +19,6 @@ class PagesController < ApplicationController
     @wallets = Wallet.where(user_id: current_user)
     amount = params[:amount].to_f
     @transaction = Transaction.new
-    # @transaction.amount = amount
-    # @transaction.sender = params[:sender]
-    # @transaction.receiver = params[:receiver]
-    # raise
-    # @transaction.save
-    # source_currency = params[:source_currency]
-    # target_currency = params[:target_currency]
-    # exchange_rate = 1.0
-
-    # converted_amount = amount * exchange_rate
-
-    # @result = {
-    #   amount: amount,
-    #   source_currency: source_currency,
-    #   target_currency: target_currency,
-    #   exchange_rate: exchange_rate,
-    #   converted_amount: converted_amount
-    # }
   end
 
   def currency
